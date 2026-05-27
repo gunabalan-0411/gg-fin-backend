214f5b27224947eb59659d58578589b3bf407fea
Gunabalan G <gunabalan0411@gmail.com>
2026-05-27 22:00:36 +0530

    update

--- alembic/versions/007_nullable_customer_in_balances.py
+++ alembic/versions/007_nullable_customer_in_balances.py
@@ -7,7 +7,7 @@ Create Date: 2026-05-27
 from typing import Sequence, Union
 from alembic import op
 
-revision: str = "007_nullable_customer_in_balances"
+revision: str = "007_nullable_balances"
 down_revision: Union[str, None] = "006_missing_tables"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
